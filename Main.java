7ebb7a89e62752faf06fafc3a840cd3af3fc5800
moustapha <moustapha1884@gmail.com>
2024-06-08 12:14:55 +0000

    function diff done

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main{
     public static void main (String[] args){
-        System.out.println("hello moustapha mbacke");
+        System.out.println(x: "hello moustapha mbacke");
+        System.out.println(x: "Calling add : ");
+        add(x:4, y:8);
     }
+}
+
+public static void add(int x, int y){
+    System.out.println(x+" + "+y+" = "+(x-y));
 }
